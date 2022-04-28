60fc27fb3afe64286ade5d13fb367ea819804a51
bert hubert <bert@hubertnet.nl>
2022-04-28 10:02:22 +0200

    make sure we get starge information ahead enough

--- get-storage.sh
+++ get-storage.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl 'https://agsi.gie.eu/api?continent=EU&from=2022-01-01&to=2022-04-24&page=1&size=365'  --fail > storage-tmp.json && jq .data < storage-tmp.json > storage-data3.json
+curl 'https://agsi.gie.eu/api?continent=EU&from=2022-01-01&to=2022-05-01&page=1&size=365'  --fail > storage-tmp.json && jq .data < storage-tmp.json > storage-data3.json
