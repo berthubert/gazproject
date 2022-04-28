3cbf5355f1f7d4a97d106f4d9f7d579208c63947
bert hubert <bert@hubertnet.nl>
2022-04-28 10:03:46 +0200

    also upload dutch-stack.png

--- update-power.sh
+++ update-power.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 rsync index-nl.html 10.0.0.4:/var/www/berthub.eu/html/nlelec/index.html
-rsync *.svg 10.0.0.4:/var/www/berthub.eu/html/nlelec/
+rsync dutch-stack.png *.svg 10.0.0.4:/var/www/berthub.eu/html/nlelec/
