607d6a0b8730a5138d4323dd955470e4b8b30333
bert hubert <bert@hubertnet.nl>
2022-08-14 21:37:00 +0200

    move to entso-e API & wrapper

--- electricity-use-clean.py
+++ electricity-use-clean.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 # coding: utf-8
 
-# In[1]:
+# In[16]:
+
 
 from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
@@ -23,128 +24,142 @@ import itertools
 
 import matplotlib.animation as animation
 import numpy as np
+from entsoe import EntsoePandasClient
 
 
-# In[2]:
+
+# In[17]:
 
 
-prefix="/home/ahu/git/gazproject/harvested/"
+# Does only realtime queries. If anything fails, exit
+
+
+# In[18]:
+
+
 imgprefix="/home/ahu/git/gazproject/"
 
 
-# In[3]:
+# In[19]:
 
 
-nleng=pandas.read_csv(prefix+"nlprod-year.csv")
-for m in range(4,6):
-    for d in range(1,31):
-        fname=prefix+"nlprod-2022"+str(m).zfill(2)+str(d).zfill(2)+".csv"
-        try: 
-            nlday=pandas.read_csv(fname)
-            print(fname)
-        except:
-            break
-        nleng=pandas.concat([nleng,nlday])
-
-nleng=nleng[nleng["Fossil Gas  - Actual Aggregated [MW]"] != "-"]
-
-nleng[["begin", "end"]] = nleng['MTU'].str.split(' - ', 1, expand=True)
-#nleng["timestamp"]=pandas.to_datetime(nleng["begin"], format="%d.%m.%Y %H:%M", utc=True)
-nleng["timestamp"]=pandas.to_datetime(nleng["begin"], format="%d.%m.%Y %H:%M", utc=True) + 0.5 * (pandas.to_datetime(nleng["end"], format="%d.%m.%Y %H:%M (UTC)", utc=True)-pandas.to_datetime(nleng["begin"], format="%d.%m.%Y %H:%M", utc=True))
+f=open("entso-e.key","r")
+lines=f.readlines()
+ekey=lines[0].strip()
+f.close()
 
 
-nleng.sort_values(["timestamp"], inplace=True)
-nleng = nleng.drop_duplicates(subset="timestamp").copy()
-
-nleng.set_index("timestamp", inplace=True)
+# In[26]:
 
 
-nleng["gaspower"]=pandas.to_numeric(nleng["Fossil Gas  - Actual Aggregated [MW]"])
-nleng["coalpower"]=pandas.to_numeric(nleng["Fossil Hard coal  - Actual Aggregated [MW]"])
-nleng["solarpower"]=pandas.to_numeric(nleng["Solar  - Actual Aggregated [MW]"])
-nleng["windpower"]=pandas.to_numeric(nleng["Wind Offshore  - Actual Aggregated [MW]"]) + pandas.to_numeric(nleng["Wind Onshore  - Actual Aggregated [MW]"])
-nleng["nukepower"]=pandas.to_numeric(nleng["Nuclear  - Actual Aggregated [MW]"])
-nleng["wastepower"]=pandas.to_numeric(nleng["Waste  - Actual Aggregated [MW]"])
-nleng["otherpower"]=pandas.to_numeric(nleng["Other  - Actual Aggregated [MW]"])
-nleng=nleng.resample("15T").interpolate("time")
-print(nleng.tail(4))
+eclient=EntsoePandasClient(api_key=ekey)
+
+start=pandas.Timestamp.today(tz="Europe/Brussels")  -pandas.Timedelta('2 days')   
+end=pandas.Timestamp.today(tz="Europe/Brussels") +pandas.Timedelta('1 days')     
+
+nleng=eclient.query_generation("NL", start=start, end=end)
+nleng["gaspower"]=nleng["Fossil Gas"]["Actual Aggregated"]
+nleng["coalpower"]=nleng["Fossil Hard coal"]["Actual Aggregated"]
+nleng["nukepower"]=nleng["Nuclear"]["Actual Aggregated"]
+nleng["solarpower"]=nleng["Solar"]["Actual Aggregated"]
+nleng["windpower"]=nleng["Wind Offshore"]["Actual Aggregated"] + nleng["Wind Onshore"]["Actual Aggregated"]
+nleng["wastepower"]=nleng["Waste"]["Actual Aggregated"]
+nleng["otherpower"]=nleng["Other"]["Actual Aggregated"] - nleng["Other"]["Actual Consumption"]
+nleng=nleng.resample("15T").interpolate(method="time")
 
 
-# In[4]:
+# In[27]:
 
 
-def addATransport(df, name):
-    nlyear = pandas.read_csv(prefix+name+"-year.csv")
-    nlyear=nlyear[nlyear[nlyear.columns[1]] != "-"] # filter out empty lines
-    nlyear=nlyear[nlyear[nlyear.columns[2]] != "-"] # filter out empty lines
-    
-    for m in range(4, 6):
-        for d in range(1, 32):
-            try:
-                newday=pandas.read_csv(prefix+name+"-2022"+str(m).zfill(2)+str(d).zfill(2)+".csv")
-                newday=newday[newday[newday.columns[1]] != "-"] # filter out empty lines
-                newday=newday[newday[newday.columns[2]] != "-"] # filter out empty lines
-    
-                nlyear=pandas.concat([nlyear, newday])
-            except:
-                break
-    nltransp=nlyear.drop_duplicates(subset=['Time (UTC)']).copy()
-                                                    
-    print(nltransp.columns[1])
-    nltransp[["begin", "end"]] = nltransp['Time (UTC)'].str.split(' - ', 1, expand=True)
-    nltransp["timestamp"]=pandas.to_datetime(nltransp["begin"], format="%d.%m.%Y %H:%M", utc=True) + 0.5 * (pandas.to_datetime(nltransp["end"], format="%d.%m.%Y %H:%M", utc=True)-pandas.to_datetime(nltransp["begin"], format="%d.%m.%Y %H:%M", utc=True))
-    nltransp.sort_values(["timestamp"], inplace=True)
-    nltransp.set_index("timestamp", inplace=True)
-    
-    nltransp[nltransp.columns[1]]=pandas.to_numeric(nltransp[nltransp.columns[1]])
-    nltransp[nltransp.columns[2]]=pandas.to_numeric(nltransp[nltransp.columns[2]])
-    nltransp=nltransp.drop("begin", axis=1)
-    nltransp=nltransp.drop("end", axis=1)
-    nltransp=nltransp.drop("Time (UTC)", axis=1)
-    
-    #plt.figure()
-    #plt.title(name)
-    #plt.plot(nltransp[nltransp.columns[0]] - nltransp[nltransp.columns[1]], '-+')
-    nltransp=nltransp.resample("15T").interpolate("time")
-    #plt.plot(nltransp[nltransp.columns[0]] - nltransp[nltransp.columns[1]])
-
-    if(len(df)==0):
-        df=nltransp
+#transp=eclient.query_import("NL", start, end)
+#transp
+# from, to
+nltransp=pandas.DataFrame()
+for c in ["DE_LU", "BE", "GB", "DK_1", "NO_2"]:
+    exp=eclient.query_crossborder_flows("NL", c, start=start, end=end).to_frame("exp "+c)
+    imp=eclient.query_crossborder_flows(c, "NL", start=start, end=end).to_frame("imp "+c)
+    res=exp.join(imp)
+    res["net "+c] = res["imp "+c] - res["exp "+c]
+    if(len(nltransp)==0):
+        nltransp = res
     else:
-        df=df.join(nltransp)
-    print(len(df))
-    return df
+        nltransp = nltransp.join(res)
 
-transp=pandas.DataFrame()
-date="year"
-transp=addATransport(transp, "nlbe")
-transp=addATransport(transp, "nldk1")
-transp=addATransport(transp, "nlde-lu")
-transp=addATransport(transp, "nlgb")
-transp=addATransport(transp, "nlno2")
-importpower=(transp["BZN|BE > BZN|NL [MW]"] + transp["BZN|DE-LU > BZN|NL [MW]"] + transp["BZN|DK1 > BZN|NL [MW]"] + transp["BZN|GB > BZN|NL [MW]"] + transp["BZN|NO2 > BZN|NL [MW]"]) -          (transp["BZN|NL > BZN|BE [MW]"] + transp["BZN|NL > BZN|DE-LU [MW]"] + transp["BZN|NL > BZN|DK1 [MW]"] + transp["BZN|NL > BZN|GB [MW]"] + transp["BZN|NL > BZN|NO2 [MW]"])
-transp
+        
+
+        
+nltransp.interpolate(method="time", inplace=True)
+nltransp["net"] = nltransp["net "+"DE_LU"]   
+for c in ["BE", "GB", "DK_1", "NO_2"]:
+    nltransp["net"] = nltransp["net"] + nltransp["net "+c]
 
 
-# In[5]:
+# In[28]:
+
+
+nltransp.net
+
+
+# In[29]:
+
+
+plt.figure()
+for c in ["DE_LU", "BE", "GB", "DK_1", "NO_2"]:
+    plt.plot(nltransp["net "+c], label=c, alpha=0.4)
+
+plt.plot(nltransp["net"], label="Total")    
+
+plt.legend()
+
+
+# In[30]:
+
+
+nlprices=eclient.query_day_ahead_prices("NL", start=start,end=end).to_frame("price")
+
+
+# In[32]:
+
+
+fig, ax1 = plt.subplots()
+sel=nlprices[pandas.Timestamp.today(tz="Europe/Brussels")  -pandas.Timedelta('1 days'):]
+
+ax1.plot(sel.index.tz_convert(tz="Europe/Amsterdam"), sel.price, label="Hourly")
+#ax1.plot(sel.price.rolling("1d").mean(center=True), label="1 day average")
+#ax1.plot(sel.price.rolling("7d").mean(center=True), label="7 day average")
+ax1.axhline(0, color='black', linestyle='-')
+
+ax2 = ax1.twinx()
+mn, mx = ax1.get_ylim() 
+ax2.set_ylim(0.1 * mn , 0.1 * mx)
+ax2.set_ylabel('cent/kWh')
+ax2.yaxis.set_major_formatter(FormatStrFormatter('%.1f'))
+
+
+ax1.grid()
+ax1.set_title("Dutch day-ahead electricity prices")
+ax1.set_ylabel("EUR/MWh")
+#ax1.set_ylim(0)
+ax1.set_xlabel("Local time")
+ax1.legend()
+plt.savefig("day-ahead.svg")
+
+
+# In[33]:
 
 
 #print(stukje.tail(10))
 plt.figure()
 
 start=pytz.utc.localize(datetime.datetime.now()-datetime.timedelta(days=2))
+print(start)
+stukje=nltransp[start:]
 
-stukje=transp[start:]
-
-plt.plot(stukje["BZN|BE > BZN|NL [MW]"] - stukje["BZN|NL > BZN|BE [MW]"], label="BE", alpha=0.4)
-plt.plot(stukje["BZN|DE-LU > BZN|NL [MW]"] - stukje["BZN|NL > BZN|DE-LU [MW]"], label="DE-LU", alpha=0.4)
-plt.plot(stukje["BZN|DK1 > BZN|NL [MW]"] - stukje["BZN|NL > BZN|DK1 [MW]"], label="DK1", alpha=0.4)
-plt.plot(stukje["BZN|GB > BZN|NL [MW]"] - stukje["BZN|NL > BZN|GB [MW]"], label="GB", alpha=0.4)
-plt.plot(stukje["BZN|NO2 > BZN|NL [MW]"] - stukje["BZN|NL > BZN|NO2 [MW]"], label="NO2", alpha=0.4)
+for c in ["DE_LU", "BE", "GB", "DK_1", "NO_2"]:
+    plt.plot(stukje["net "+c], label=c, alpha=0.4)
 
 
-
-plt.plot(importpower[start:],
+plt.plot(stukje.net,
         label="Total", lw=2)
 plt.grid()
 plt.legend()
@@ -155,7 +170,7 @@ plt.xlabel("UTC")
 plt.savefig(imgprefix+"nl-exports.svg")
 
 
-# In[6]:
+# In[34]:
 
 
 plt.figure()
@@ -176,9 +191,9 @@ plt.stackplot(restr.index,
 #plt.plot((restr["nukepower"]+restr["windpower"]+restr["coalpower"]+restr["gaspower"]+restr["otherpower"]).rolling("1h", center=True).mean(), label="Daily averaged sum")
 #plt.plot((restr["nukepower"]+restr["windpower"]+restr["coalpower"]+restr["gaspower"]+restr["otherpower"]+importpower).rolling("1h", center=True).mean(), label="Daily averaged sum imp")
 
-plt.plot(importpower[start:], color='red', label="Electricity imports")
+plt.plot(nltransp[start:].net, color='red', label="Electricity imports/exports")
 #plt.legend(loc=2)
-labels = labels + list(["Electricity imports"])
+labels = labels + list(["Electricity imports/exports"])
 
 plt.legend(reversed(plt.legend().legendHandles), reversed(labels), loc=2)
 #plt.gca().legend(handles[::-1], labels[::-1])#, loc='upper left')
@@ -191,37 +206,37 @@ plt.title("Dutch known electricity generation by source")
 plt.savefig(imgprefix+"known-generation.svg")
 
 
-# In[7]:
+# In[35]:
 
 
 plt.figure()
-labels=["Imports", "Nuke", "Coal", "Gas", "Other", "Waste"]
+labels=["Imports/Exports", "Nuke", "Coal", "Gas", "Other", "Waste"]
 start=pytz.utc.localize(datetime.datetime.now()-datetime.timedelta(days=2))
 
-restr=nleng[start:importpower.index.max()]
-resimportpower=importpower[restr.index.min():restr.index.max()]
+restr=nleng[start:nltransp.index.max()]
+resnltransp=nltransp[restr.index.min():restr.index.max()]
 
-print(len(restr[:resimportpower.index.max()]))
-print(len(resimportpower[start:]))
-print(len(restr[:resimportpower.index.max()]["nukepower"]))
-print(len(restr[:resimportpower.index.max()]["coalpower"]))
+print(len(restr[:resnltransp.index.max()]))
+print(len(resnltransp[start:]))
+print(len(restr[:resnltransp.index.max()]["nukepower"]))
+print(len(restr[:resnltransp.index.max()]["coalpower"]))
 
-plt.stackplot(restr[:resimportpower.index.max()].index,
-              resimportpower[start:],
-              restr[:resimportpower.index.max()]["nukepower"],
-              restr[:resimportpower.index.max()]["coalpower"],restr[:resimportpower.index.max()]["gaspower"],
+plt.stackplot(restr[:resnltransp.index.max()].index,
+              resnltransp[start:].net,
+              restr[:resnltransp.index.max()]["nukepower"],
+              restr[:resnltransp.index.max()]["coalpower"],restr[:resnltransp.index.max()]["gaspower"],
 
-                restr[:resimportpower.index.max()]["otherpower"], restr[:resimportpower.index.max()]["wastepower"], 
+                restr[:resnltransp.index.max()]["otherpower"], restr[:resnltransp.index.max()]["wastepower"], 
               labels=labels,
               colors=['grey', 'orange', 'black', 'steelblue', 'purple', 'brown']
              )
 
 #plt.plot((restr["nukepower"]+restr["windpower"]+restr["coalpower"]+restr["gaspower"]+restr["otherpower"]).rolling("1h", center=True).mean(), label="Daily averaged sum")
-#plt.plot((restr["nukepower"]+restr["windpower"]+restr["coalpower"]+restr["gaspower"]+restr["otherpower"]+resimportpower).rolling("1h", center=True).mean(), label="Daily averaged sum imp")
+#plt.plot((restr["nukepower"]+restr["windpower"]+restr["coalpower"]+restr["gaspower"]+restr["otherpower"]+resnltransp).rolling("1h", center=True).mean(), label="Daily averaged sum imp")
 
-plt.plot(resimportpower[start:], color='red', label="Electricity imports")
+plt.plot(resnltransp[start:].net, color='red', label="Electricity imports/exports")
 #plt.legend(loc=2)
-labels = labels + list(["Electricity imports"])
+labels = labels + list(["Electricity imports/exports"])
 
 plt.legend(reversed(plt.legend().legendHandles), reversed(labels), loc=2)
 #plt.gca().legend(handles[::-1], labels[::-1])#, loc='upper left')
@@ -239,53 +254,14 @@ plt.savefig(imgprefix+"dutch-stack.svg")
 plt.savefig(imgprefix+"dutch-stack.png")
 
 
-# In[8]:
-
-
-plt.figure()
-labels=["Export", "Import", "Gas"]#, "Nuke", "Coal", "Wind", "Gas", "Other", "Waste"]
-restr=nleng.join(importpower.to_frame())
-start=datetime.datetime.now()-datetime.timedelta(days=2)
-
-small=restr[start:]
-respower =(small["nukepower"]+small["coalpower"]+small["gaspower"]+small["otherpower"]+small["wastepower"])+small[0]
-plt.plot(small[0], color='r', label="Net imports")
-plt.bar(small.index, np.maximum(small[0], 0), width=1/(24*4.0), label="Imports", color='grey')
-w=1.18/(24*4.0)
-plt.bar(small.index, small["nukepower"], bottom=small[0], width=w, label="Nuclear", color='orange')
-plt.bar(small.index, small["coalpower"], bottom=small[0]+small["nukepower"], width=w, label="Coal", color='black')
-plt.bar(small.index, small["gaspower"], bottom=small[0]+small["nukepower"]+small["coalpower"], width=w, color='steelblue', label="Gas")
-plt.bar(small.index, small["otherpower"], bottom=small[0]+small["nukepower"]+small["gaspower"]+small["coalpower"], width=w, label="Other", color='purple')
-plt.bar(small.index, small["wastepower"], bottom=small[0]+small["otherpower"]+small["nukepower"]+small["gaspower"]+small["coalpower"], width=w, label="Waste", color='brown')
-
-
-handles, labels = plt.gca().get_legend_handles_labels()
-plt.gca().legend(handles[::-1], labels[::-1])#, loc='upper left')
-plt.gcf().text(0.95, 0.6, "NL\n↑", fontsize=16, horizontalalignment='center')
-plt.gcf().text(0.95, 0.2, "↓\nBE DE\nNO UK\nDK", fontsize=16, horizontalalignment='center')
-
-
-plt.grid()
-plt.xlabel("UTC")
-plt.ylabel("MW")
-plt.title("Dutch non-renewable electricity generation by source")
-plt.axhline(y=0, color='black', linestyle='-')
-
-
-# In[ ]:
-
-
-
-
-
-# In[9]:
+# In[36]:
 
 
 #print(importpower.resample("15T").interpolate("time"))
-knownpower=(nleng["nukepower"]+nleng["coalpower"]+nleng["gaspower"]+nleng["otherpower"]+nleng["wastepower"])+importpower
+knownpower=(nleng["nukepower"]+nleng["coalpower"]+nleng["gaspower"]+nleng["otherpower"]+nleng["wastepower"])+nltransp.net
 # +nleng["windpower"] ++
 #knownpower=knownpower["2022-04-01":"2022-04-25"]
-print(knownpower.head(10))
+print(knownpower.tail(10))
 plt.figure()
 #
 kpdf=knownpower.to_frame()
@@ -293,8 +269,8 @@ kpdf["time"]=pandas.to_datetime(kpdf.index.time.astype(str))
 hh_mm = DateFormatter('%H:%M')
 plt.gca().xaxis.set_major_formatter(hh_mm)
 
-for dnum in [23,30]:
-    daystr="2022-04-"+str(dnum).zfill(2)
+for dnum in [13,14]:
+    daystr="2022-08-"+str(dnum).zfill(2)
     day=kpdf.loc[daystr]
     plt.plot(day.time, day[0], label=daystr, alpha=0.9)
 
@@ -313,6 +289,39 @@ plt.xlabel("UTC")
 
 plt.axhline(y=0, color='black', linestyle='-')
 plt.savefig(imgprefix+"nlduck.svg")
+
+
+# In[37]:
+
+
+plt.figure()
+
+restr=restr[start:]
+plt.plot(restr["gaspower"], label="Gas", color='steelblue')
+plt.plot(restr["coalpower"], label="Coal", color='black')
+plt.plot(restr["windpower"], label="Some of the wind", color='green')
+plt.plot(restr["solarpower"], label="Tiny bit of solar", color='yellow', lw=2)
+#plt.plot(restr["solarpower"]*90, label="Estimate of solar", color='yellow', lw=2)
+
+plt.plot(restr["nukepower"], label="Nuclear",color='orange')
+plt.plot(restr["wastepower"], label="Waste", color='brown')
+plt.plot(restr["otherpower"], label="Other", color='purple')
+
+plt.grid()
+plt.ylim(1)
+plt.yscale("log")
+plt.ylabel("MW")
+plt.title("ENTSO-E / TenneT Dutch known/reported electricity production")
+#plt.axhline(y=restr.nukepower.quantile(0.99), color='black', linestyle='-')
+plt.xlabel("UTC")
+plt.legend()
+plt.savefig(imgprefix+"tennet-total.svg")
+
+
+# In[ ]:
+
+
+
 
 
 # In[ ]:
